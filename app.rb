77bf1156de7ac41f083fd0398e7de66af0b4d402
Chris Kerbo <chris.kerbo@outlook.com>
2019-11-11 21:49:49 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,6 +5,6 @@ class App < Sinatra::Base
 
   configure do
     enable :sessions
-    set :session_secret, SecureRandom.hex(64)
+    set :session_secret, ENV.fetch('SESSION_SECRET') {SecureRandom.hex(64)}
   end
 end
